ab799c0c0a31db2a59d49cfdf84cd7c56241d8c2
sampi0l <54300639+sampi0l@users.noreply.github.com>
2019-10-19 18:58:59 +0700

    Add files via upload

---
+++ waitForElement.js
@@ -0,0 +1,10 @@
+const waitForElement = (element) => {
+    return new Promise((res, err) => {
+        const check = setInterval(() => {
+            if(document.querySelectorAll(element) !== []) {
+                clearInterval(check);
+                res(document.querySelectorAll(element));
+            }
+        }, 300)
+    })
+}
