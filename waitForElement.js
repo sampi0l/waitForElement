8e343de92d0b982977cf85709753a7adf74aecab
sampi0l <54300639+sampi0l@users.noreply.github.com>
2019-10-19 19:03:59 +0700

    Update waitForElement.js

--- waitForElement.js
+++ waitForElement.js
@@ -1,7 +1,7 @@
 const waitForElement = (element) => {
     return new Promise((res, err) => {
         const check = setInterval(() => {
-            if(document.querySelectorAll(element) !== []) {
+            if([...document.querySelectorAll(element)] !== []) {
                 clearInterval(check);
                 res(document.querySelectorAll(element));
             }
